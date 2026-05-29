bd69e8b5c0cde8917292e72aa45727aeec4d8030
Priyanshu Sharma <145048420+priyanshu442004@users.noreply.github.com>
2026-05-29 14:45:20 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -21,7 +21,8 @@ app.use(
       "https://audit-tool-one.vercel.app",
       "https://audit-tool-ipri.vercel.app",
       "https://audit-tool-new.vercel.app",
-      "https://audit-tool-bfn1.vercel.app"
+      "https://audit-tool-bfn1.vercel.app",
+      "https://audit-eight-tau.vercel.app/"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
